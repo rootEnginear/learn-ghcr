c345b4f7dbc9564843dcca9bd72b583d4bc7b206
rootEnginear <rootenginear@users.noreply.github.com>
2025-08-25 18:08:36 +0700

    feat: update ci, more paths to play

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get("/random", (c) => {
+  return c.json({ result: Math.random() });
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
